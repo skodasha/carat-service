b58ce90c6570a0d04fd0ffd55de92c8dddd0dc29
skodasha <skodasha@users.noreply.github.com>
2020-06-21 14:13:13 +0300

    Updates

---
+++ precache-manifest.530df408cc9f61da325f097da90c1f2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91680ca14a8a5cfff7c6b52df0dc48a",
+    "url": "/carat-service/index.html"
+  },
+  {
+    "revision": "ff946943aeb4a1d8fff9",
+    "url": "/carat-service/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc80bc28babe52f2de42",
+    "url": "/carat-service/static/js/2.7706098b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carat-service/static/js/2.7706098b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff946943aeb4a1d8fff9",
+    "url": "/carat-service/static/js/main.7260f91c.chunk.js"
+  },
+  {
+    "revision": "fa2aa54fa7578f509e2d",
+    "url": "/carat-service/static/js/runtime-main.50c86ae1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carat-service/static/media/logo.5d5d9eef.svg"
+  }
+]);
