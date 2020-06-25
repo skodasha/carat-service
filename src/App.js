246d138a3fce9b695a673201de3e921ee4bd1f6d
skodasha <skodasha@users.noreply.github.com>
2020-06-25 12:56:40 +0300

    add hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Main from './pages/Main';
@@ -7,12 +7,12 @@ import Catalog from './pages/Catalog';
 
 function App() {
   return (
-    <BrowserRouter basename={`${process.env.PUBLIC_URL}/`}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <Switch>
-      <Route path='/' exact component={Main}/>
-      <Route path='/catalog' component={Catalog}/>
+      <Route path='/catalog' exact component={Main}/>
+      <Route path='/' component={Catalog}/>
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 
   );
 }
