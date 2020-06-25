2901ea3f22a99fd56befc4bcbe0e8422c801bc95
skodasha <skodasha@users.noreply.github.com>
2020-06-25 12:50:46 +0300

    Updates

--- precache-manifest.9d4d1a4138a7f90b93f534ce3c466a45.js
+++ precache-manifest.9d4d1a4138a7f90b93f534ce3c466a45.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6dd22b5dec2c6ca0d8e171dfd3fc350f",
+    "revision": "c357ac564013947d8dfd0d23accef59f",
     "url": "/carat-service/index.html"
   },
   {
-    "revision": "a130af6dcd777fa01004",
+    "revision": "cd81088c0be78479f864",
     "url": "/carat-service/static/css/main.51fc57b0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/carat-service/static/js/2.db038b09.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a130af6dcd777fa01004",
-    "url": "/carat-service/static/js/main.4bf199ff.chunk.js"
+    "revision": "cd81088c0be78479f864",
+    "url": "/carat-service/static/js/main.35f4acf8.chunk.js"
   },
   {
     "revision": "fa2aa54fa7578f509e2d",
